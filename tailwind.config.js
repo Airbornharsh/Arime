7f07bd43678afcc79ca017a6a532bf0ee6e9ff43
Harsh Keshri <harshkeshri1234567@gmail.com>
2022-11-01 18:20:49 +0530

    Silly Error

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ require("@tailwindcss/typography");
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
